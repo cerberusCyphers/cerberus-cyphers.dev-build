863e4806591e890d8314a7a4036665f4cdeb7ffc
Craig Colwell <ccolwell0683@gmail.com>
2021-04-10 22:24:45 -0400

    new parcel build for re-publish mobile site

---
+++ index.8d9fd458.js
@@ -0,0 +1,2 @@
+!function(){const c=document.querySelector(".hamburger"),e=document.querySelector(".main-nav");console.log(c),c.addEventListener("click",(function(c){e.classList.toggle("hidden"),console.log("click")})),e.addEventListener("click",(function(c){e.classList.add("hidden"),console.log("click")}))}();
+//# sourceMappingURL=index.8d9fd458.js.map
