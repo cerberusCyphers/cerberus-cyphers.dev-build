827494772fc204d54d622010683b4060a055b922
Craig Colwell <ccolwell0683@gmail.com>
2021-04-14 10:24:34 -0400

    working on media queries

--- index.8d9fd458.js
+++
@@ -1,2 +0,0 @@
-!function(){const c=document.querySelector(".hamburger"),e=document.querySelector(".main-nav");console.log(c),c.addEventListener("click",(function(c){e.classList.toggle("hidden"),console.log("click")})),e.addEventListener("click",(function(c){e.classList.add("hidden"),console.log("click")}))}();
-//# sourceMappingURL=index.8d9fd458.js.map
